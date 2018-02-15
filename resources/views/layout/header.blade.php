517729c0c1387a78f3c03aefc4cedfa99247b92a
Someone <some@one.com>
2018-02-15 16:29:39 +0800

    update in front

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,12 +1,27 @@
 <div class="header">
 	<div class="topbar">
 		<label class="userIDlabel float-left">UserID</label>
-		<div class="float-right">
-			<a href="/home" class="topbarlink">Home</a>
-			<a href="/bets" class="topbarlink">Open Bets</a>
-			<a href="/bets/history" class="topbarlink">History</a>
-			<a href="/diposit" class="topbarlink">Deposit</a>
-		</div>
+		<nav class="navbar navbar-expand-lg navbar-light  float-right">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="/home" class="topbarlink">Home</a>
+					</li>
+					<li class="nav-item">
+						<a href="/bets" class="topbarlink">Open Bets</a>
+					</li>
+					<li class="nav-item">
+						<a href="/bets/history" class="topbarlink">History</a>
+					</li>
+					<li class="nav-item">
+						<a href="/diposit" class="topbarlink">Deposit</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	</div>
 	<div class="secondbar">
 		<div class="row">
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,52 +2,52 @@
 	<div class="card-body">
 		<div class="title" >Past Events : <strong>Feb 15, 2018</strong></div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>	
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>	
 		<div class="event">
-			<div row> {Past Date} </div>
-			<div row> EventID: {EID} </div>
-			<div row> EventName: {Ename} </div>
-			<div row> Winner: {Wname} </div>
+			<div row> Feb 14, 2018 </div>
+			<div row> EventID: EVT_MOTTO_28 </div>
+			<div row> EventName: Motor Racing in HK </div>
+			<div row> Winner: Bla Bla </div>
 		</div>																						
 	</div>
 </div>
